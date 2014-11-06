540d000ef256fc2fd89b2d02a1fc9b92c3ca97b9
Sean Darcy <darcys22@gmail.com>
2014-11-06 03:21:53 -0800

    copied over

--- game/states/play.js
+++ game/states/play.js
@@ -1,12 +1,63 @@
 
   'use strict';
+var Shift = require('../prefabs/shift');
+
+var SHIFT_SIZE = 71;
+var SHIFT_HEIGHT = 40;
+
   function Play() {}
   Play.prototype = {
     create: function() {
+  this.setup(); 
     },
     update: function() {
-
+  if (game.input.mousePointer.justReleased()) 
+    {
+      this.shiftAdd(null,null);
     }
+    },
+
+setup:  function()  {
+  this.game.stage.backgroundColor = 0x333333;
+  this.shiftGrid = this.game.add.group();
+  this.input.justReleasedRate = 25;
+
+  this.ruler();
+  this.buttons();
+
+},
+
+shiftAdd:  function(sprite, pointer) {
+  var hour = Math.floor(this.game.input.x/71);
+  var shift = new Shift(hour);
+  this.shiftGrid.add(shift);
+},
+
+buttons: function() {
+  var date = "February 1st 2014"
+  var style = { font: "40px Arial", fill: "#9CA2B8" };
+  var tdate = game.add.text(10, 10, date, style);
+
+  previus = this.game.add.button(10, 65, 'previous');
+  clear = this.game.add.button(10, 110, 'clear', this.clearBut);
+},
+
+clearBut: function() {
+  this.shiftGrid.removeAll()
+},
+
+ruler: function() {
+  var rlrbdr = this.game.add.bitmapData(this.game.width, 50);
+  //rlrbdr context is a html5 canvas context so what you draw with that yeah
+  rlrbdr.ctx.strokeStyle = "#9CA2B8";
+  rlrbdr.ctx.lineWidth=5;
+  rlrbdr.ctx.beginPath();
+  rlrbdr.ctx.moveTo(0,0);
+  rlrbdr.ctx.lineTo(game.width,0);
+  rlrbdr.ctx.stroke();
+
+  this.floor = this.game.add.sprite(0,this.game.height-50,rlrbdr);
+},
   };
   
   module.exports = Play;
